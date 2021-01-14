a136d299d88a055ae85fcdf5e376c9e0080a99ba
gpadem <gulcepadem@gmail.com>
2021-01-14 17:32:57 +0100

    First day of the project, classes and methods established

--- utils/card.py
+++ utils/card.py
@@ -1,12 +1,12 @@
 class Symbol(self,color,icon):
     """class defining a symbol by its two attributes color and icon"""
     def __init__(self,color,icon):
-        self.color = ""
+        self.color = ["Red","Black"]
         self.icon = [♥, ♦, ♣, ♠]
 
 class Card(Symbol):
     """class that inherits from the class Symbol and gets a new attribute about its value"""
     def __init__(self,color,icon,value):
         Symbol.__init__(self,color,icon)
-        self.value
+        self.value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
---
+++ utils/player.py
@@ -0,0 +1,48 @@
+from card.py import Card
+
+class Player(self,cards,turn_count=0,number_of_cards=0,history):
+    """class that defines a player by the following attributes: 
+    cards the player has at hand, the number of player's turn, 
+    the amount of cards the player has, 
+    the history of cards used by the player"""  
+    def __init__(self,cards,turn_count=0,number_of_cards=0,history):
+        self.cards=Card
+        self.turn_count= turn_count
+        self.number_of_cards=
+        self.history=[]
+
+    def play(self,)
+    """Pick a card, add it to the history of a player, print something, return the card"""
+        import random
+        self.cards
+        print (f "{Player} {turn_count} played: {value} {icon}")
+        return Card
+
+
+class Deck(self,cards):
+    def __init__(self,cards)
+        self.cards= Card
+
+    def fill_deck(self):
+        """create a full deck"""
+
+
+    def shuffle():
+        """shuffle all the list of cards"""
+
+
+    def distribute():
+        """take Player as a parameter and distribute cards evenly between all players"""
+
+
+
+
+
+
+
+
+
+
+
+
+
